449e71610a7cfccb86f18cd15ad51792973aad60
ChrisLai <chrislai@vt.edu>
2015-09-02 14:32:29 -0400

    Hello World for Node.js app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// Figure how to run high port on AWS
+var port = process.env.PORT || 1337;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 80);
+app.listen(port);
+console.log("Node server running on port " + port);
